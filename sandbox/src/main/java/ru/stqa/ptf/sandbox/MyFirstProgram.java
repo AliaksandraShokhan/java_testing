2b37b6c94d596f1a2ad93902a4f727cfac8517d0
Alexandra <alexandra.shokhan@gmail.com>
2020-03-14 19:30:09 +0300

    New methods added

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,9 +1,30 @@
 package ru.stqa.ptf.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello("world");
+        hello("Alexandra");
 
-        System.out.println("Hello world!");
-}
+        double len = 4;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return Math.pow(len, 2);
+    }
+
+    public static double area(double a, double b) {
+        return  a * b;
+    }
 }
