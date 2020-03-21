2f54bd2d8d25fe6963ff38a2f5823fcb4bae0f69
Alexandra <alexandra.shokhan@gmail.com>
2020-03-21 21:27:08 +0300

    Project structure updated

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
@@ -1,14 +1,20 @@
 package ru.stqa.ptf.addressbook;
 
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTests {
   private WebDriver wd;
 
-  @BeforeMethod (alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
