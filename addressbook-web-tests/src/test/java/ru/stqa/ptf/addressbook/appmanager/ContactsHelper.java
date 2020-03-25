300f7f2d61eec5aca2acb7c094fc8103aee92a46
Alexandra <alexandra.shokhan@gmail.com>
2020-03-25 23:48:13 +0300

    Contact modification test added.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactsHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactsHelper.java
@@ -59,4 +59,45 @@ public class ContactsHelper extends HelperBase {
     public void initContactCreation() {
         click(By.linkText("add new"));
     }
+
+    public void initContactModification(int id) { click(By.xpath("(//img[@alt='Edit'])[" + id + "]")); }
+
+    public void submitContactModification() { click((By.name("update"))); }
+
+    public void returnToHomePage() { click(By.linkText("home page")); }
+
+    public void editContactForm(ContactData contactData) {
+        type(By.name("firstname"), contactData.getFirstName());
+        type(By.name("middlename"), contactData.getMiddleName());
+        type(By.name("lastname"), contactData.getLastName());
+        type(By.name("nickname"), contactData.getNickName());
+        type(By.name("company"), contactData.getCompany());
+        type(By.name("title"), contactData.getTitle());
+        type(By.name("address"), contactData.getAddress());
+        type(By.name("home"), contactData.getHomePhone());
+        type(By.name("mobile"), contactData.getMobilePhone());
+        type(By.name("work"), contactData.getWorkPhone());
+        type(By.name("fax"), contactData.getFax());
+        type(By.name("email"), contactData.getEmail1());
+        type(By.name("email2"), contactData.getEmail2());
+        type(By.name("email3"), contactData.getEmail3());
+        type(By.name("homepage"), contactData.getHomepage());
+        click(By.name("bday"));
+        select(By.name("bday"), contactData.getBirthdayDay());
+        click(By.name("bmonth"));
+        select(By.name("bmonth"), contactData.getBirthdayMonth());
+        click(By.name("byear"));
+        click(By.name("byear"));
+        type(By.name("byear"), contactData.getBirthdayYear());
+        click(By.name("aday"));
+        select(By.name("aday"), contactData.getAnniversDay());
+        click(By.name("amonth"));
+        select(By.name("amonth"), contactData.getAnniversMonth());
+        click(By.name("ayear"));
+        type(By.name("ayear"), contactData.getAnniversYear());
+        click(By.name("address2"));
+        type(By.name("address2"), contactData.getAddressExtra());
+        type(By.name("phone2"), contactData.getHomePhoneExtra());
+        type(By.name("notes"), contactData.getNotes());
+    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.ptf.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+    @Test
+    public void testContactModification() {
+        app.getContactsHelper().initContactModification(3);
+        app.getContactsHelper().editContactForm(new ContactData("123", "456", "789",
+                "nickname", "title", "company", "address", "hometelephone",
+                "mobiletelephone", "work telephone", "fax", "email@sample.com",
+                "email2@sample.com", "email@sample.com", "www.site.org", "2",
+                "April", "1990", "6", "April", "1900",
+                "test1", "address", "home", "notes"));
+        app.getContactsHelper().submitContactModification();
+        app.getContactsHelper().returnToHomePage();
+    }
+}
