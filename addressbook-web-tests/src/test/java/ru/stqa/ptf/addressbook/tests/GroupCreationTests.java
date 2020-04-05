fcbeb20a69381c62f2d94c760e488afa63379cf0
Alexandra <alexandra.shokhan@gmail.com>
2020-04-05 23:18:02 +0300

    Assertions for group creation test added.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
@@ -7,9 +8,13 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
+        int before = app.getGroupsHelper().getGroupCount();
 
         app.getNavigationHelper().goToGroupsPage();
         app.getGroupsHelper().createGroup(new GroupData("test1", null, null));
+
+        int after = app.getGroupsHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
         app.logout();
     }
 }
