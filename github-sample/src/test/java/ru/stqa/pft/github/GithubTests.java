c6f52febc8e36d5a4fbb4fe852ebad4a2b3f6d04
Alexandra <alexandra.shokhan@gmail.com>
2020-05-15 12:28:39 +0300

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("b3cd8eee9ee426c50f0fbda62e803b5532a7d341");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("AliaksandraShokhan", "java_testing"))
+                .commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
